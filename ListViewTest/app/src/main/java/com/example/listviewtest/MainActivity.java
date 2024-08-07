02b2dc3971c9ce3e2f151374d1931b2a620c6d36
D1204493 <161418128+D1204493@users.noreply.github.com>
2024-08-07 11:15:02 +0800

    透過自訂ArrayAdapter的方式，讓ListView的內容能自動變化

--- ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.java
+++ ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.listviewtest;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,7 +15,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    private ListView cityListView;
+    private List<String> cities = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,20 +34,25 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             return insets;
         });
 
-        ListView lv = (ListView) findViewById(R.id.cityListView);
-        lv.setOnItemClickListener(this);
+//        ListView lv = (ListView) findViewById(R.id.cityListView);
+//        lv.setOnItemClickListener(this);
+
+        cityListView = (ListView) findViewById(R.id.cityListView);
+        setCities();
+        cityListView.setOnItemClickListener(this);
     }
 
 
+    //覆寫 onItemClick 方法
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
         TextView tv = (TextView) findViewById(R.id.textView);
-        String[] citiesArray = getResources().getStringArray(R.array.cities);
+//        String[] citiesArray = getResources().getStringArray(R.array.cities);  //設橋接器(Adapter)出現錯誤訊息：ArrayIndexOutOfBoundsException。因為使用了 ArrayList<>(較為彈性)，所以這一行註解掉。
 
         //使用AlertDialog
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         dialog.setTitle("城市");
-        dialog.setMessage("您選擇的是：" + citiesArray[i]);
+        dialog.setMessage("您選擇的是：" + cities.get(i));  //設橋接器(Adapter)出現錯誤訊息：ArrayIndexOutOfBoundsException。把 citiesArray[i] 調整成 cities.get(i)
         dialog.setCancelable(true);
         //dialog.show();
         //也可以這樣使用：dialog.setTitle("城市").setMessage("您選擇的是：" + citiesArray[i]).setCancelable(true).show();
@@ -58,5 +70,19 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         //使用TextView
 //        tv.setText("您選擇的是：" + citiesArray[i]);
     }
+
+    //橋接器(Adapter)方法
+    private void setCities() {
+        //先讀入原本六都的縣市
+        cities = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.cities)));
+        //新增縣市
+        cities.add("Hualien");
+        cities.add("Taitung");
+        cities.add("Changhua");
+        //new ArrayAdapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, cities);
+        cityListView.setAdapter(adapter);
+    }
+
 
 }
